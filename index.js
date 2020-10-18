c2a5daf677cb44020a8566c88b7956b4fe1793b3
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-10-18 17:35:45 -0300

    Adição de Resolução.

---
+++ index.js
@@ -0,0 +1,6 @@
+let nomeusuario = prompt ("Por favor, digite seu nome: ");
+let sobrenomeusuario = prompt ("Por favor, digite seu sobrenome: ");
+let sobrenomecatalogo = sobrenomecatalogo.toUpperCase();
+
+alert (`Seu nome completo é: ${nomeusuario} ${sobrenomeusuario}.`);
+alert (`Seu nome de catálogo é: ${sobrenomecatalogo}, ${nomeusuario}.`);
